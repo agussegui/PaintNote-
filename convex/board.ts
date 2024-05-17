af21edabd1065b40646ac77cd949eebac8db1bf8
AgustinSegui <agussegui24@hotmail.es>
2024-05-17 16:43:40 -0300

    New board you have Components the canvas, a new authorization in convex for the create,favorites,remove,update and search boards.Installation liveblocks, create API and route.ts for authentication the session in Liveblocks, request processing, verifies that the dashboard's orgId matches the authenticated user's orgId also return 403 error. If exists a room, full acceess is allowed a that room for the session.

--- convex/board.ts
+++ convex/board.ts
@@ -1,7 +1,6 @@
 import {v} from "convex/values"
 
-import {mutation} from "./_generated/server"
-import { title } from "process";
+import {mutation, query} from "./_generated/server"
 
 const images = [
     "/placeholders/1.svg",
@@ -160,8 +159,17 @@ export const unfavorite = mutation({
             throw new Error("Favorited board not found")
         }  
 
-        await ctx.db.delete(existingFavorite._id    )
+        await ctx.db.delete(existingFavorite._id)
 
         return board 
+    }
+})
+
+export const get = query({
+    args: {id: v.id("boards")},
+    handler: async(ctx, args) => {
+        const board = ctx.db.get(args.id)
+
+        return board;
     }
 })
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const COLORS = [
+  '#0A18AC',
+  '#d10e0e',
+  '#069c1b',
+  '#c10ccf',
+  '#21e1de'
+]
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function connectionIdToColor(connectionId: number): string{
+  return COLORS[connectionId % COLORS.length]
 }
